b7e504b7d8c03c5c30eabb904c909ad28363b809
Maarten Sander <maarten.sander@gingerpayments.nl>
2019-10-02 20:46:49 +0200

    Exclude tests and examples from distribution.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gingerpayments/ginger-python",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests', 'examples')),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
